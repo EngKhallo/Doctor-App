ef085b9c7f659c9f7cd06de6c70f2e251b0aea93
Sir Khalid <engkhallo.ict@gmail.com>
2022-01-22 16:20:46 +0300

    first commit

---
+++ src/components/Message.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+let notify = 'Dynamic Text';
+</script>
+
+<template>
+    <h3 class="text-purple-600 text-3xl font-bold">{{notify}}</h3>
+
+    <button class="button">Login</button>
+    <div class="my-5"></div>
+    <button class="button">Signup</button>
+
+</template>
